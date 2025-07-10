997d9b0eb4d9d6b7dfd76905c3f519e31d95514e
Ismael R. Valenzuela <ismaelv@protonmail.com>
2025-07-09 21:13:59 -0700

    Passport can fully authenticate and the userContext is being shared with Socket.io server.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from "express";
 import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
@@ -8,6 +7,12 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import { Strategy as localStrategy } from "passport-local";
 import dotenv from "dotenv";
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+  RequestHandler,
+} from "express";
 
 declare global {
   namespace Express {
@@ -40,6 +45,28 @@ app.use(passport.session());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+passport.use(
+  new localStrategy((username, password, done) => {
+    if (username === "Ish" && password === "Ish") {
+      console.log("authentication was a success");
+      return done(null, { id: 1, username });
+    } else {
+      console.log("failed to authenticate");
+      return done(null, false);
+    }
+  })
+);
+
+passport.serializeUser((user, cb) => {
+  console.log(`serializeser ${user.id}`);
+  cb(null, user);
+});
+
+passport.deserializeUser((user: Express.User, cb) => {
+  console.log(`deserializeUser ${user.id}`);
+  cb(null, user);
+});
+
 app.get("/", (req, res) => {
   if (!req.user) {
     return res.redirect("/login");
@@ -65,31 +92,48 @@ app.post(
 app.post("/logout", (req, res) => {
   const sessionId = req.session.id;
   req.session.destroy(() => {
+    // disconnect all Socket.IO connections linked to this session ID
     io.to(`session:${sessionId}`).disconnectSockets();
     res.status(204).end();
   });
 });
 
-passport.use(
-  new localStrategy((username, password, done) => {
-    if (username === "Ish" && password === "tempPassword") {
-      console.log("authentication was a success");
-      return done(null, { id: 1, username });
+function onlyForHandshake(middleware: RequestHandler) {
+  return (
+    req: Request & { _query: Record<string, string> },
+    res: Response,
+    next: NextFunction
+  ) => {
+    const isHandshake = req._query.sid === undefined;
+    if (isHandshake) {
+      middleware(req, res, next);
     } else {
-      console.log("failed to authenticate");
-      return done(null, false);
+      next();
+    }
+  };
+}
+io.engine.use(onlyForHandshake(sessionMiddleware));
+io.engine.use(onlyForHandshake(passport.session()));
+io.engine.use(
+  onlyForHandshake((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.writeHead(401);
+      res.end();
     }
   })
 );
 
-passport.serializeUser((user, cb) => {
-  console.log(`serializeser ${user.id}`);
-  cb(null, user);
-});
+io.on("connection", (socket) => {
+  const req = socket.request as Request & { user: Express.User };
 
-passport.deserializeUser((user: Express.User, cb) => {
-  console.log(`deserializeUser ${user.id}`);
-  cb(null, user);
+  socket.join(`session:${req.session.id}`);
+  socket.join(`user:${req.user.id}`);
+
+  socket.on("whoami", (cb) => {
+    cb(req.user.username);
+  });
 });
 
 httpServer.listen(port, () => {
